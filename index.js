04ed6560f8d8c595cc5c90793051d0f4e1dc1b94
nixer89 <pbnixer@gmail.com>
2019-06-04 18:19:39 +0200

    test

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const i2c = require('i2c-bus');
 
-const i2c1 = i2c.openSync(1);
-i2c1.writeByteSync(0x53,0x21,0xb);
-let data = [];
-let d;
-do {
-	d=i2c1.readByteSync(0x53,0x20);
-	if (d) data.push(String.fromCharCode(d));
-} while (d);
-console.log(data.join(""));
-//i2c1.close();
+
+readNfcData();
+writeNfcData();
+readNfcData();
+
+function readNfcData() {
+
+	const i2c1 = i2c.openSync(1);
+	i2c1.writeByteSync(0x53,0x21,0xb);
+	let data = [];
+	let d;
+
+	do { 
+		d = i2c1.readByteSync(0x53,0x20);
+		if (d) data.push(String.fromCharCode(d));
+	} while (d);
+
+	console.log(data.join(""));
+}
+
+function writeNfcData() {
+	const newText = "instant.dncash.io?triggercode=das-ist-ein-test";
+	const byteBuffer = Buffer.from(newText);
+
+	const i2c1 = i2c.openSync(1);
+	for(let i = 0; i < byteBuffer.length;i++) {
+		i2c1.writeByteSync(0x53,0x21,byteBuffer[i]);
+	}
+}
