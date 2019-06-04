67ca209ac265aa6c33eb0bfbf4594796efed052a
nixer89 <pbnixer@gmail.com>
2019-06-04 17:18:14 +0200

    read write implemented

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const i2c = require('i2c-bus');
 
 const i2c1 = i2c.openSync(1);
-console.log(i2c1.readByteSync(0x53,0x20));
-i2c1.close();
+i2c1.writeByteSync(0x53,0x21,0xb);
+let data = [];
+let d;
+do {
+	d=i2c1.readByteSync(0x53,0x20);
+	if (d) data.push(String.fromCharCode(d));
+} while (d);
+console.log(data.join(""));
+//i2c1.close();
